6158df125ed4f0b48789f1fa85a17c869efb87e1
Igor <igordecastro07@gmail.com>
2022-04-10 21:39:17 -0300

    randomizar cartas

--- scripts.js
+++ scripts.js
@@ -1,20 +1,13 @@
-let gifs = [
-    "bobrossparrot",
-    "explodyparrot",
-    "fiestaparrot",
-    "metalparrot",
-    "revertitparrot",
-    "tripletsparrot",
-    "unicornparrot"
-];
+let gifs = [1, 2, 3, 4, 5, 6, 7];
 gifs.sort(comparador);
 
 let shuffleCards = [];
 function comparador() {
     return Math.random() - 0.5;
 }
-const carta = document.querySelector(".cartas1")
-const carta2 = document.querySelector(".cartas2")
+const carta = document.querySelector(".cartas1");
+const carta2 = document.querySelector(".cartas2");
+let escolhidos = [];
 
 let CardQtd = prompt("Com quantas cartas você quer jogar?");
 
@@ -33,18 +26,26 @@ function AddCard(i) {
     <img src="images/${gifs[i]}.gif">
     </div>
     `
+}
+
+
+for (let i = 0; i < halfqtd; i++) {
+    escolhidos.push(gifs[i]);
+    AddCard(i);
+} 
+
+escolhidos.sort(comparador);
+
+for (let i = 0; i < halfqtd; i++) {
     carta2.innerHTML += `<div class="carta" onclick="virarCarta(this)">
     <div class="frente cartaConteudo">
     <img src="images/front.png">
     </div>
     <div class="tras cartaConteudo">
-    <img src="images/${gifs[i]}.gif">
+    <img src="images/${escolhidos[i]}.gif">
     </div>
     `
 }
-for (let i = 0; i < halfqtd; i++) {
-    AddCard(i); 
-} 
 
 function virarCarta(elemento) {
     elemento.classList.add("selecionada")
